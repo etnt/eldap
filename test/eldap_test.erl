e309de4db4b78d67d6232f5bbaa2ff8e34ecc069
Torbjorn Tornkvist <tobbe@myggan.(none)>
2010-04-12 20:33:49 +0200

    Removing the GPL text. It is now replaced with the MIT license.

--- test/eldap_test.erl
+++ test/eldap_test.erl
@@ -7,20 +7,6 @@
 %%% Copyright (c) 2010 Torbjorn Tornkvist <tobbe@tornkvist.org>
 %%% See MIT-LICENSE at the top dir for licensing information.
 %%% 
-%%% This program is free software; you can redistribute it and/or modify
-%%% it under the terms of the GNU General Public License as published by
-%%% the Free Software Foundation; either version 2 of the License, or
-%%% (at your option) any later version.
-%%%
-%%% This program is distributed in the hope that it will be useful,
-%%% but WITHOUT ANY WARRANTY; without even the implied warranty of
-%%% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-%%% GNU General Public License for more details.
-%%%
-%%% You should have received a copy of the GNU General Public License
-%%% along with this program; if not, write to the Free Software
-%%% Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-%%%
 %%% --------------------------------------------------------------------
 -vc('$Id$ ').
 -export([topen_bind/1,topen_bind/2,all/0,t10/0,t20/0,t21/0,t22/0,
